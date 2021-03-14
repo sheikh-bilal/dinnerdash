2fd24de3b96d564e95e5122484fd8d44f17c202e
bilalahm3d <b.ahmad@devsinc.com>
2021-03-15 01:22:41 +0500

    UI updated: Items Menu or forms Updated

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,4 @@
 class Item < ApplicationRecord
   validates :title, presence: true, length: { minimum: 3, maximum: 30 }
-  validates :desc, presence: true, length: { minimum: 10, maximum: 70 }
+  validates :desc, presence: true, length: { minimum: 10, maximum: 150 }
 end
