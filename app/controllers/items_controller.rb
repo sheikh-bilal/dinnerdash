82904648284a22f724c2821c751827da77866064
bilalahm3d <b.ahmad@devsinc.com>
2021-03-15 11:50:35 +0500

    items update: Added image uploading func to food items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -45,6 +45,6 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:title, :price, :desc)
+    params.require(:item).permit(:title, :price, :image, :desc)
   end
 end
