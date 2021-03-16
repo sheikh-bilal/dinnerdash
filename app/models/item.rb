f2c14a3119abd66c0d18cf702c9d7e61c40996c4
bilalahm3d <b.ahmad@devsinc.com>
2021-03-17 02:03:22 +0500

    AddedValiation: Added items validation and updation

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,5 +3,7 @@ class Item < ApplicationRecord
   has_many :categories, through: :item_categories
   validates :title, presence: true, length: { minimum: 3, maximum: 30 }
   validates :desc, presence: true, length: { minimum: 10, maximum: 150 }
+  validates :price, presence: true, numericality: { greater_than: 0 }
+  validates_uniqueness_of :title
   mount_uploader :image, AvatarUploader
 end
