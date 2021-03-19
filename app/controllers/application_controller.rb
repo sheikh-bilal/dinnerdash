55ded0b109e0a3f099e4255ebc201977dd85173b
bilalahm3d <b.ahmad@devsinc.com>
2021-03-19 12:44:12 +0500

    Updated: changes suggested by Buddy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    # Permit the `subscribe_newsletter` parameter along with the other
-    # sign up parameters.
+
     devise_parameter_sanitizer.permit(:sign_up, keys: [:fname, :dname])
     devise_parameter_sanitizer.permit(:account_update, keys: [:fname, :dname])
   end
