7d533ab9e7d4d60a70752d72eddc9f0e4321fd65
bilalahm3d <b.ahmad@devsinc.com>
2021-03-25 15:28:22 +0500

    OrderPolicy: Implement order Policy

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,13 +2,18 @@ class OrdersController < ApplicationController
 
   before_action :set_order, only: [:edit, :show]
   def index
-    @orders = Order.all
+    if user_signed_in? && current_user.admin?
+      @orders = Order.all
+    else
+      @orders = current_user.orders
+    end
   end
 
   def edit
   end
 
   def show
+    authorize @order
   end
 
   private
--- app/policies/order_policy.rb
+++ app/policies/order_policy.rb
@@ -3,4 +3,16 @@ class OrderPolicy < ApplicationPolicy
   def create?
     return true if user.present?
   end
+
+  def show?
+    return true if (user.present? and (record.user_id == user.id))
+  end
+
+  def showUI?
+    return true if user.present? and user.admin?
+  end
+
+  def edit?
+    return true if user.present? and user.admin?
+  end
 end
