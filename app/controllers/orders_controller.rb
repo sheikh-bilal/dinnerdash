4789ad9ca360322dfc674a81629848eb36c929ff
bilalahm3d <b.ahmad@devsinc.com>
2021-03-25 12:05:35 +0500

    AddUser: Assign orders to user

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,19 @@
 class OrdersController < ApplicationController
 
+  before_action :set_order, only: [:edit, :show]
   def index
     @orders = Order.all
   end
 
+  def edit
+  end
+
   def show
+  end
+
+  private
+
+  def set_order
     @order = Order.find(params[:id])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :orders, dependent: :destroy
   #Devise::email_regexp is currently defined as: /\A[^@\s]+@[^@\s]+\z/ which doesn't validate all emails so i am using my own
 
   validates :fname, presence: true, length: {minumum: 3, maximum: 25}
