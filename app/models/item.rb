06c756b840fdd054c1b956dbcb983bc42decfd66
bilalahm3d <b.ahmad@devsinc.com>
2021-03-31 18:32:14 +0500

    one-liner code for custom validation(updated)

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,13 +9,8 @@ class Item < ApplicationRecord
   validates :title, presence: true, length: { minimum: 3, maximum: 30 }, uniqueness: true
   validates :desc, presence: true, length: { minimum: 10, maximum: 150 }
   validates :price, presence: true, numericality: { greater_than: 0 }
-  validate :must_have_one_item
+  validate { errors.add(:base, 'You must select at least one category') if category_ids.all?(&:blank?) }
 
   mount_uploader :image, AvatarUploader
 
-  private
-
-  def must_have_one_item
-    errors.add(:base, 'You must select at least one category') if category_ids.all?(&:blank?)
-  end
 end
