2c8a3e6606fb58d8e4216b2903eda41f85173bd3
bilalahm3d <b.ahmad@devsinc.com>
2021-04-06 14:12:43 +0500

    Update helper: added transection

--- app/helpers/order_items_helper.rb
+++ app/helpers/order_items_helper.rb
@@ -4,13 +4,19 @@
 module OrderItemsHelper
   def copydata(order)
     current_cart.cart_items.each do |cart_item|
-      order.order_items.create!(
+      @check = order.order_items.create(
         quantity: cart_item.quantity,
         item_id: cart_item.item_id,
         order_id: order.id,
         total: cart_item.total,
         unit_price: cart_item.unit_price
       )
+      if @check.save
+        flash[:notice] = 'Order saved successfully'
+      else
+        flash[:alert] = "Error: Order not saved"
+        redirect_to carts_path
+      end
     end
   end
 end
